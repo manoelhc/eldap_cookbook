b00b116aa9c1103ce7625e10038944074eeab1cc
Manoel Carvalho <manoelhc@gmail.com>
2017-06-15 00:58:30 +0200

    Add URLs

--- eldap/metadata.rb
+++ eldap/metadata.rb
@@ -10,14 +10,16 @@ gem 'net-ldap', '= 0.16.0'
 
 depends 'openldap', '~> 3.0.3'
 
+source_url 'https://github.com/manoelhc/eldap_cookbook'        if respond_to?(:source_url)
+issues_url 'https://github.com/manoelhc/eldap_cookbook/issues' if respond_to?(:issues_url)
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/manoelhc/eldap_cookbook/issues' if respond_to?(:issues_url)
+#
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/manoelhc/eldap_cookbook' if respond_to?(:source_url)
